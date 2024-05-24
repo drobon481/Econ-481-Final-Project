1b79a071ee3292b57490e526471b82b2ca45296b
zachkoverman <zachkoverman@gmail.com>
2024-05-23 21:40:32 -0700

    Zach: starting EDA in jupyter notebook, setting up .py file

---
+++ wage_growth_analysis.py
@@ -0,0 +1,31 @@
+"""
+David Robon, Henry Tran, Zach Koverman
+Lukas Hager
+29 May 2024
+ECON 481 - Data Science Computing for Economics
+Final Project
+
+This file contains functions for cleaning data and conducting analysis on wage 
+growth in the United States from 1977 through 2024.
+
+Data source:
+Federal Reserve Bank of Atlanta
+https://www.atlantafed.org/chcs/wage-growth-tracker
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def get_github() -> str:
+    """
+    Returns the URL to our GitHub repository for this project.
+    """
+    return 'https://github.com/drobon481/Econ-481-Final-Project'
+
+def main():
+    """
+    Calls our functions to test them.
+    """
+
+if __name__ == "__main__":
+    main()
